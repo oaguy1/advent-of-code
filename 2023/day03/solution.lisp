6cd1b7c3d9ce302ce6a43c25e63fb3fd81c830d9
Lily Hughes-Robinson <oaguy1@gmail.com>
2023-12-19 06:05:22 -0500

    Finish 2023 Day 3

--- 2023/day03/solution.lisp
+++ 2023/day03/solution.lisp
@@ -36,12 +36,14 @@
 	((= j j-max) nil)
       (do ((i 0 (+ 1 i)))
 	  ((= i i-max) nil)
+	(princ (aref arr j i))
 	(if (eql #\* (aref arr j i))
-	    (append gear-ratios (list (gear-ratio arr nums i j))))))
+	    (setf gear-ratios (append gear-ratios (list (gear-ratio nums i j)))))))
+    (pprint gear-ratios)
     (reduce #'+ gear-ratios)))
 
 (defun init-nums (arr)
-  (let ((nums (make-array (list (array-dimension arr 0) (array-dimension arr 1))))
+  (let ((nums (make-array (list (array-dimension arr 0) (array-dimension arr 1)) :initial-element -1))
 	(i-max (array-dimension arr 1))
 	(j-max (array-dimension arr 0))
 	(curr-num-buff "")
@@ -141,68 +143,87 @@
 
     (reduce #'(lambda (x y) (or x y)) (map 'list #'car results))))
 
-(defun gear-ratio (arr nums i j)
-  ;; skip all this work if the character isn't a number
-  (when (not (digit-char-p (aref arr j i)))
-    (return-from gear-ratio nil))
-  
+(defun gear-ratio (nums i j)
   (let ((results '())
-	(max-i (- (array-dimension arr 1) 1))
-	(max-j (- (array-dimension arr 0) 1)))
+	(max-i (- (array-dimension nums 1) 1))
+	(max-j (- (array-dimension nums 0) 1))
+	(top-left -1)
+	(top-center -1)
+	(top-right -1)
+	(left -1)
+	(right -1)
+	(bottom-left -1)
+	(bottom-center -1)
+	(bottom-right -1))
 
-    ;; diagonally up and to the left, don't search if we are the next digit
+    ;; diagonally up and to the left
     (let ((x (- i 1))
-	  (y (- j 1))) 
-      (unless (or (or (= i 0) (= j 0)))
-        (setf results (append results (list (list (special-char-p (aref arr y x)) :diagonal-up-left))))))
-
-    ;; directly to the left, could be a prev digit
-    (let ((x (- i 1))
-	  (y j))
-      (unless (or (= i 0))
-	(if (digit-char-p (aref arr y x))
-	    (setf results (append results (list (list (is-connected-p arr x y :prev-digit t) :behind))))
-	    (setf results (append results (list (list (special-char-p (aref arr y x)) :behind)))))))
-
-    ;; diagonally down and to the left, don't search if we are the next digit
-    (let ((x (- i 1))
-	  (y (+ j 1)))
-      (unless (or (or (= j max-j) (= i 0)))
-        (setf results (append results (list (list (special-char-p (aref arr y x)) :diagonal-down-left))))))
+	  (y (- j 1)))
+      (unless (or (= i 0) (= j 0))
+	(setf top-left (aref nums y x))
+	(unless (< top-left 0)
+	  (setf results (append (list top-left) results)))))
 
     ;; directly above
     (let ((x i)
 	  (y (- j 1)))
       (unless (= j 0)
-        (setf results (append results (list (list (special-char-p (aref arr y x)) :above))))))
+	(setf top-center (aref nums y x))
+	(unless (or (< top-center 0) (= top-center top-left))
+	  (setf results (append (list top-center) results)))))
+
+    ;; diagonally up and to the right
+    (let ((x (+ i 1))
+	  (y (- j 1)))
+      (unless (or (= i max-i) (= j 0))
+	(setf top-right (aref nums y x))
+	(unless (or (< top-right 0) (= top-right top-center))
+	  (setf results (append (list top-right) results)))))
+
+    ;; directly to the left, could be a prev digit
+    (let ((x (- i 1))
+	  (y j))
+      (unless (or (= i 0))
+	(setf left (aref nums y x))
+	(unless (< left 0)
+	  (setf results (append (list left) results)))))
+
+    ;; directly to the right, could be a next digit
+    (let ((x (+ i 1))
+	  (y j))
+      (unless (= i max-i)
+	(setf right (aref nums y x))
+	(unless (< right 0)
+	  (setf results (append (list right) results)))))
+
+    ;; diagonally down and to the left, don't search if we are the next digit
+    (let ((x (- i 1))
+	  (y (+ j 1)))
+      (unless (or (= j max-j) (= i 0))
+	(setf bottom-left (aref nums y x))
+	(unless (< bottom-left 0)
+	  (setf results (append (list bottom-left) results)))))
 
     ;; directly below
     (let ((x i)
 	  (y (+ j 1)))
       (unless (= j max-j)
-        (setf results (append results (list (list (special-char-p (aref arr y x)) :below))))))
-
-    ;; diagonally up and to the right
-    (let ((x (+ i 1))
-	  (y (- j 1)))
-      (unless (or (= i max-i) (= j 0) prev-digit)
-        (setf results (append results (list (list (special-char-p (aref arr y x)) :diagonal-up-right))))))
+	(setf bottom-center (aref nums y x))
+	(unless (or (< bottom-center 0) (= bottom-center bottom-left))
+	  (setf results (append (list bottom-center) results)))))
 
     ;; diagonally down and to the right
     (let ((x (+ i 1))
 	  (y (+ j 1)))
-      (unless (or (= j max-j) (= i max-i) prev-digit)
-        (setf results (append results (list (list (special-char-p (aref arr y x)) :diagonal-down-right))))))
+      (unless (or (= j max-j) (= i max-i))
+	(setf bottom-right (aref nums y x))
+	(unless (or (< bottom-right 0) (= bottom-right bottom-center))
+	  (setf results (append (list bottom-right) results)))))
  
-    ;; directly to the right, could be a next digit
-    (let ((x (+ i 1))
-	  (y j))
-      (unless (or (= i max-i) prev-digit)
-	(if (digit-char-p (aref arr y x))
-	    (setf results (append results (list (list (is-connected-p arr x y :next-digit t) :right))))
-	    (setf results (append results (list (list (special-char-p (aref arr y x)) :right)))))))
 
-    (reduce #'(lambda (x y) (or x y)) (map 'list #'car results))))
+    (if (= 1 (length results))
+	0
+	(reduce #'* results))))
 
 (defun special-char-p (ch)
   (cond ((eql ch #\.) nil)
